f83d4309382d75b247f3eac550b72a71382f670c
railsfactory-rajalakshmi <rajalakshmis@railsfactory.org>
2013-09-04 15:45:53 +0530

    Got roman numbers till 10

--- lakshmi/4thsept/roman.rb
+++ lakshmi/4thsept/roman.rb
@@ -1,9 +1,26 @@
 class Roman
 def self.convert(number)
-if(number==1)
-  'I'
-else
-  'II'
+case number
+when 1
+'I'
+when 2
+'II'
+when 3
+'III'
+when 4
+'IV'
+when 5
+'V'
+when 6
+'VI'
+when 7
+'VII'
+when 8
+'VIII'
+when 9
+'IX'
+when 10
+'X'
 end
 end
 end
