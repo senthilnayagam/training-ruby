bd92f4f0c28dd48fe8d8c3147f5321c7f4ba05c5
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-05 15:07:14 +0530

    Code works for all numbers

--- shekar/5_September/prime.rb
+++ shekar/5_September/prime.rb
@@ -2,10 +2,16 @@
 class Prime
     def check_prime(num)
         if num.class == "Fixnum"
-	 if (num % 2) == 0
- 	  return "True"
+	 c=0
+         for i in 1..num
+	 if (num % i) == 0
+            c+=1	
+	 end
+	 end
+	 if c==2
+		return "True"
 	 else
- 	  return "False"
+		return "False"
 	 end
 	else return "Invalid"
 	end
