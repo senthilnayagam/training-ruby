5d64cd74e2320f5b85cf62362efd745b826029ab
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-05 13:49:57 +0530

    prime and prime test files

--- shekar/05_September/prime_test.rb
+++ shekar/05_September/prime_test.rb
@@ -3,7 +3,7 @@ require "test/unit"
 require "./prime"
 
 class Prime_Test < Test::Unit::TestCase
-  def test_conversion
+  def check_prime
    assert_equal('True', Prime.check_prime(1) )
    #assert_equal('I',Roman.convert('a'))
    
