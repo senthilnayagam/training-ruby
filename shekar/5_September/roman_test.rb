f2d9dc6251b01277d513f77170539d96521923d1
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-05 16:21:05 +0530

    Code works for 3 digit numbers into roman

--- shekar/5_September/roman_test.rb
+++ shekar/5_September/roman_test.rb
@@ -5,7 +5,6 @@ class TestRoman < Test::Unit::TestCase
   def test_conversion
    assert_equal('I', Roman.convert(1) )
    assert_equal('Invalid data',Roman.convert('a'))
-=begin 
    assert_equal('II',Roman.convert(2))
    assert_equal('III',Roman.convert(3))
    assert_equal("IV",Roman.convert(4))
@@ -17,7 +16,13 @@ class TestRoman < Test::Unit::TestCase
    assert_equal("X",Roman.convert(10))
    assert_equal("XI",Roman.convert(11))
    assert_equal("XII",Roman.convert(12))
-=end
+  end
+  def test_conversion1
+   assert_equal('CLI', Roman.convert(151) )
+   #assert_equal('Invalid data',Roman.convert(xxx))
+   #assert_equal('II',Roman.convert(299))
+   #assert_equal('III',Roman.convert(3000))
+   #assert_equal("IV",Roman.convert(004))
   end
 end
 
