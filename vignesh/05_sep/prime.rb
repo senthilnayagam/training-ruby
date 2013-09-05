4378d3bf9f157ae9109b0ca5c5185544789849af
railsfactory-anandvignesh <anandvignesh@railsfactory.org>
2013-09-05 15:47:57 +0530

    Prime Number coding created

---
+++ vignesh/05_sep/prime.rb
@@ -0,0 +1,20 @@
+class Prime_number
+
+
+ def get_value
+ i = gets.to_i
+ prime = true
+ for counter in 2..(Math.sqrt(i)) do
+ if i % counter == 0 then
+ prime = false
+ end
+ end
+ if prime then
+ puts "True"
+ else
+ puts "False"
+ end
+ end
+end
+a = Prime_number.new
+a.get_value
