d56721e9cb2b3bc6ac6d3d91c0d3464a0051b680
Marishree <marishree@railsfactory.org>
2013-09-05 14:41:43 +0530

    calculator with add sub mul div

---
+++ shree/5thsept/calc_test.rb
@@ -0,0 +1,13 @@
+require "test/unit"
+require "./calc.rb"
+
+class Testcalc < Test::Unit::TestCase
+ 
+  def test_conversion
+    assert_equal(10, Calc.add(5,5) )
+    assert_equal(3, Calc.sub(5,2) )
+    assert_equal(4, Calc.mul(2,2) )
+    assert_equal(2, Calc.div(8,4) )
+    
+  end
+end
