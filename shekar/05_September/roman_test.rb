f234e1b34b7b28131a89db13db4597ac6491c9f7
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-05 13:13:56 +0530

    Prime number program

---
+++ shekar/05_September/roman_test.rb
@@ -0,0 +1,25 @@
+require "test/unit"
+require "./roman"
+
+class TestRoman < Test::Unit::TestCase
+ 
+  def test_conversion
+   assert_equal('I', Roman.convert(1) )
+   #assert_equal('I',Roman.convert('a'))
+   assert_equal('II',Roman.convert(2))
+   assert_equal('III',Roman.convert(3))
+   assert_equal("IV",Roman.convert(4))
+   assert_equal("V",Roman.convert(5))
+   assert_equal("VI",Roman.convert(6))
+   assert_equal("VII",Roman.convert(7))
+   assert_equal("VIII",Roman.convert(8))
+   assert_equal("IX",Roman.convert(9))
+   assert_equal("X",Roman.convert(10))
+   assert_equal("XI",Roman.convert(11))
+   assert_equal("XII",Roman.convert(12))
+  end
+
+end
+
+
+
