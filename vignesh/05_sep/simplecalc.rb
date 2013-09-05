f8b95fecb77d68f1464858e22098c0fc4a106ee6
railsfactory-anandvignesh <anandvignesh@railsfactory.org>
2013-09-05 14:45:37 +0530

    simple calc coding part

--- vignesh/05_sep/simplecalc.rb
+++ vignesh/05_sep/simplecalc.rb
@@ -1,9 +1,24 @@
-class SimpleCalculator
+class Simplecalculator
 
-  def plus(a, b)
-    a + b
-  end
-
-  def minus(a, b)
-    a - b
-  end
+def self.addition(a,b)
+sum=a+b
+sum
+end
+def self.subtraction(a,b)
+sub=a-b
+sub
+end
+def self.multiply(a,b)
+mul=a*b
+mul
+end
+def self.divide(a,b)
+if(b!=0)
+div= "Denominator should not be zero"
+div
+else
+div=a/b
+div
+end
+end
+end
