cfeca052451cbbd4e24435819891e9e483969813
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-06 14:27:47 +0530

    Code inserts multiple records to table

--- shekar/06_September/connect_db.rb
+++ shekar/06_September/connect_db.rb
@@ -4,6 +4,11 @@ begin
 
 db1=Mysql.new('localhost','root','root','college')
 #db1.query("create table test_student(sname varchar(25),age integer,grade char(1))")
-ds=db1.query("insert into test_student values('Chandra sekaran R',23,'A')")
+db1.query("insert into test_student values('Ganga',23,'B')")
+db1.query("insert into test_student values('Raman',22,'A')")
+db1.query("insert into test_student values('Nataraj',21,'C')")
+db1.query("insert into test_student values('Ram kumar',22,'C')")
+db1.query("insert into test_student values('Raghavan',22,'B')")
+
 end
 
