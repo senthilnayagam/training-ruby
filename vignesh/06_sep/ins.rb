24cc29c3abfb6147d99650f1c49f9c5022ca5f07
railsfactory-anandvignesh <anandvignesh@railsfactory.org>
2013-09-06 17:06:58 +0530

    Inserted

---
+++ vignesh/06_sep/ins.rb
@@ -0,0 +1,18 @@
+#Program to connect to database and perform basic DML operation.
+
+require 'mysql'
+begin
+
+db=Mysql.new('localhost','root','root','college')
+
+db.query("insert into testfile values('Ajay',21,'A')")
+db.query("insert into testfile values('Aswin',21,'A')")
+db.query("insert into testfile values('Amit',21,'C')")
+db.query("insert into testfile values('Arun',21,'C')")
+db.query("insert into testfile values('Anand',21,'B')")
+
+ds=db.query("Select * from testfile")
+ds.each do |i|
+puts i.join(" ")
+end
+end
