2f06a6b599fa1fc4fde54f1981751b385b14a844
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-06 16:10:17 +0530

    Code to update record fetched from user@runtime in table

--- shekar/06_September/update_record.rb
+++ shekar/06_September/update_record.rb
@@ -11,7 +11,18 @@ db1.query("insert into test_student values('Raman',22,'A')")
 db1.query("insert into test_student values('Nataraj',21,'C')")
 db1.query("insert into test_student values('Ram kumar',22,'C')")
 db1.query("insert into test_student values('Raghavan',22,'B')")
-=end
-
 db1.query("update test_student set grade='B' where sname like 'Ram%'")
+=end
+puts "Input student data to be updated : "
+print "Name  : "
+name=gets
+print "Age   : "
+age=gets.to_i
+print "Grade : "
+grade=gets
+
+qry="update test_student set sname='#{name}',age=#{age.to_i},grade='#{grade}')"
+db1.query(qry)
+puts "#{name} record inserted"
+
 end
