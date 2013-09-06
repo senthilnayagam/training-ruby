20d8c5086e20feccca120fe3550238516bcba4c5
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-06 14:32:13 +0530

    Code selects and prints the records in the table

--- shekar/06_September/connect_db.rb
+++ shekar/06_September/connect_db.rb
@@ -3,12 +3,17 @@ require 'mysql'
 begin
 
 db1=Mysql.new('localhost','root','root','college')
-#db1.query("create table test_student(sname varchar(25),age integer,grade char(1))")
+=begin
+db1.query("create table test_student(sname varchar(25),age integer,grade char(1))")
 db1.query("insert into test_student values('Ganga',23,'B')")
 db1.query("insert into test_student values('Raman',22,'A')")
 db1.query("insert into test_student values('Nataraj',21,'C')")
 db1.query("insert into test_student values('Ram kumar',22,'C')")
 db1.query("insert into test_student values('Raghavan',22,'B')")
-
+=end
+ds=db1.query("Select * from test_student order by grade asc")
+ds.each do |i|
+puts i.join(" ")
+end
 end
 
