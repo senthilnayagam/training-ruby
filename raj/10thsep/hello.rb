a8a90fb363862ba2837071679e31a3a9c0f59aae
railsfactory-rajendraprasad <rajendraprasad@railsfactory.org>
2013-09-10 18:29:33 +0530

    file added

--- raj/10thsep/hello.rb
+++ raj/10thsep/hello.rb
@@ -8,19 +8,34 @@ get '/hi' do
 end
 
 get '/time' do
-  Time.now.to_s
-end
-
+     Time.now.to_s
+   end
+   
 get '/hello' do
 name=params[:name]
-  "hello " + name.to_s
+ "hello " + name
 end
-
 
 get '/rollno' do
-con=Mysql.new 'localhost','root','root','college'
-res=con.query("select * from student where rno=101")
-res.each do |row|
-puts row
+ # con=Mysql.new 'localhost','root','root','college'
+  #rollno=params[:rollno]
+  #query="select * from student where rno=" + rollno.to_s
+  #res=con.query(query)
+  
+  res=  "<br>"
+  student={ "rno" => "raj",
+                   "name" => "102",
+                   "sub1" => "bio",
+                   "marks" => "89",
+                   "result" => "pass"
+                    }.each do |key,value|
+    
+res += key.to_s + " "
+
+res += value.to_s + "<br>"
+
+
+
 end
+  res
 end
