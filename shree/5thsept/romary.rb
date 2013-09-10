1bf381e124989944add32c3501d962b346dc5bee
Marishree <marishree@railsfactory.org>
2013-09-10 11:51:11 +0530

    modified

---
+++ shree/5thsept/romary.rb
@@ -0,0 +1,15 @@
+class Roman
+def convert(number)
+b=[0,1,2,3,4,5,6,7,8,9,10]
+a= [' ','I','II','III','IV','V','VI','VII','VIII','IX','X']
+
+for i in 1..10
+if (number == b[i])
+print a[i]+"\n"
+end
+end
+end
+end
+r=Roman.new
+r.convert(10)
+
