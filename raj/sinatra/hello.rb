9f3ec2c6de9fc5bcf68c40d4f98f960e68ba0885
railsfactory-rajendraprasad <rajendraprasad@railsfactory.org>
2013-09-10 18:48:45 +0530

    updated

--- raj/sinatra/hello.rb
+++ raj/sinatra/hello.rb
@@ -1,7 +1,27 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
+require 'mysql'
 
 get '/hi' do
   "Hello World!"
+end
+
+get '/time' do
+     Time.now.to_s
+   end
+   
+get '/hello' do
+name=params[:name]
+ "hello " + name
+end
+
+get '/rollno' do
+  con=Mysql.new 'localhost','root','root','college'
+  rollno=params[:rollno]
+  query="select * from student where rno=" + rollno.to_s
+  res=con.query(query)
+  res.each do |row|
+    puts row
+  end
 end
