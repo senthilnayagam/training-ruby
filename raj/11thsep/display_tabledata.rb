cb8641481e48592f5ce4a71eac65309e86bb2d1b
railsfactory-rajendraprasad <rajendraprasad@railsfactory.org>
2013-09-11 16:48:45 +0530

    to display table data

--- raj/11thsep/display_tabledata.rb
+++ raj/11thsep/display_tabledata.rb
@@ -8,9 +8,31 @@ get '/table' do
   #rollno=params[:rollno]
   query="select * from student"
   res=con.query(query)
-  result= " "
+  result= "<table border=1>
+	    <tr>
+		<th>RollNo</th>	
+		<th>Name</th>
+		<th>DOB</th>
+		<th>Sub1</th>
+		<th>Marks1</th>
+		<th>Sub2</th>
+		<th>Marks2</th>
+		<th>sub3</th>
+		<th>Marks3</th>
+		<th>Sub4</th>
+		<th>Marks4</th>
+		<th>Sub5</th>
+		<th>Marks5</th>
+		<th>Sub6</th>	
+		<th>Marks6</th>
+		<th>Total</th>
+		<th>Avg</th>
+		<th>University</th>
+		<th>Result</th>
+	   </tr>"
+		
   res.each do |row|
-    result += row.to_s + " " + "<br>"
+    result += "<tr><td>" + row.join("</td><td>") + "</td></tr></table>"
   end
-result
+ result 
 end
