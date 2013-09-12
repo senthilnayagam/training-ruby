9c6ab1e02ac9974e8addeedcb2a3028d248c2c7b
railsfactory-rajalakshmi <rajalakshmis@railsfactory.org>
2013-09-12 17:09:02 +0530

    created table included styles

--- lakshmi/11sept/sinatra/hello.rb
+++ lakshmi/11sept/sinatra/hello.rb
@@ -6,13 +6,36 @@ require 'mysql'
 get '/rollno' do 
 con = Mysql.new 'localhost', 'root', 'root', 'test1'
 rollno = params[:rollno]
-head="<html><title>Result</title><head><script>
+head="<html><title>Result</title><head><link rel=\"stylesheet\" type=\"text/css\" href=\"/mystyle.css\"></link>
+<style>
+
+table
+{
+background-color:#D8BFD8;
+color:#4B0082;
+}
+marquee
+{
+color:	#008B8B;
+font-size:35;
+}
+h1{
+color:#FFFFFF;
+font-family: arial, verdana, sans-serif;
+font-size:35px;
+}
+</style>
+<script>
 function goBack()
   {
   window.history.back()
   }
-</script><marquee>Your result is </marquee></head><body>"
-str="<input type=\"button\" value=\"Back\" onclick=\"goBack()\">"
+</script>
+<br/>
+<marquee>Your result is </marquee><br/><br/>
+</head><body style=\"background-color:#4B0082;\">"
+
+str="<center><input type=\"button\" value=\"Back\" onclick=\"goBack()\" ></center>"
 
 foot="</body></html>"
 result=' '+" <table border=1>
@@ -41,7 +64,7 @@ result=' '+" <table border=1>
 qry="SELECT * FROM student where rollno="+rollno.to_s;
 rs = con.query(qry)
 rs.each do |row|
-result +="<tr><td>"+ row.join("</td><td>") + "</td></tr></table>" 
+result +="<tr><td>"+ row.join("</td><td>") + "</td></tr></table><br/><br/></br>" 
 end
 head+result+str+foot
 end
@@ -50,12 +73,24 @@ get '/result' do
 string="<HTML>
 <title>	Result :</title>
 <head><h1> Result</h1></head>
-<BODY>
+<style>
+body{
+margin-left:300px;
+color:	#008B8B;
+}
+h1{
+color:	#FFFFFF;
+font-family: arial, verdana, sans-serif;
+font-size:35px;
+}
+</style>
+<BODY bgcolor=\"#483D8B\" background=\"/imag1.jpg\">
 <FORM action=\"/rollno\" method=\"get\">
-
-ROLL NO: <input type=\"TEXT\" name=\"rollno\">
- <input type=\"submit\" value=\"Submit\">
-
+<fieldset style=\"text-align:center; width: 500px; height: 100px;\">
+<legend>Result : </legend>
+ROLL NO: <input type=\"TEXT\" name=\"rollno\"><br/><br/>
+<input type=\"submit\" value=\"Submit\">
+</fieldset>
 </FORM >
 </BODY>
 </HTML>"
