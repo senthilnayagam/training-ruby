af2643fc2f24c9aa276c25cbe8fcf79955c3156b
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-12 12:51:09 +0530

    Code modified to print student result in html page

--- shekar/12_September/gem_code1.rb
+++ shekar/12_September/gem_code1.rb
@@ -8,7 +8,7 @@ get '/hi' do
 end
 
 get '/result_info' do
- k ="<html><head><title>University of Madras Result 2013 page</title></head><center><body><form><br><h3>University of Madras Results 2013 (PG) - MCA (PG)</h3><br><br>"
+ k ="<html><head><title>University of Madras Result 2013 page</title></head><center><body><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><form><br><h3>University of Madras Results 2013 (PG) - MCA (PG)</h3><br><br>"
  k+="<table border=1 bgcolor=grey size=1000><tr><th>Roll No</th><th>Name</th><th>Date of Birth</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Total</th><th>Average</th><th>University</th><th>Year</th></tr>"
 
   rollno=params[:rollno]
@@ -24,20 +24,23 @@ get '/result_info' do
     va="No such roll number exists!"
     return va
   end
-  k+="</table><center><br><br><a href=\'/result'\"><i>Back to home</i></a></form></body></html>"
+  k+="</table><center><br><br><a href=\'/result'\"><i>Back to home</i></a></form><p><ul>
+<li>This is a computer gerenated result and hence are prone to system error.</li>
+<li>This web site may not gaureenty the precision of subject marks.</li>
+</ul></p></body></html>"
   return k
 end
 
 get '/result' do
 s="<html><head><title>University of Madras Result 2013 page</title></head>
-<body><marquee bgcolor=\"yellow\"><font color=red><b>University id Madras PG Results 2013</b></font></marquee><center><br><h3>University of Madras Results 2013 (PG) - MCA</h3><br>
+<body><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><center><br><h3>University of Madras Results 2013 (PG) - MCA</h3><br>
 <form id=\"login\" action=\"/result_info\" method=\"get\">
-<table border=\"1\" size=\"500\">
+<table border=\"1\" size=\"700\">
 <tr><td align=\"center\">
 Roll Number : <input type=\"text\" name=\"rollno\"><br><br>
 Click here <input type=\"submit\" value=\"submit\" name=\"submit\">
 </td></tr>
-</table>
+</table><br><br>
 </form>
 </center>
 </body>
