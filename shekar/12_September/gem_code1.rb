e30545c70de50cdcfd2001191df68195f565f379
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-12 15:44:10 +0530

    code modified

--- shekar/12_September/gem_code1.rb
+++ shekar/12_September/gem_code1.rb
@@ -8,7 +8,7 @@ get '/hi' do
 end
 
 get '/result_info' do
- k ="<html><head><title>University of Madras Result 2013 page</title></head><center><body><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><form><br><h3>University of Madras Results 2013 (PG) - MCA (PG)</h3><br><br>"
+ k ="<html><head><title>University of Madras Result 2013 page</title></head><center><body><img src=\"/home/chandrasekar/Chan_Programs/mm.png\"><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><form><br><h3>University of Madras Results 2013 (PG) - MCA (PG)</h3><br><br>"
  k+="<table border=1 bgcolor=grey size=1000><tr><th>Roll No</th><th>Name</th><th>Date of Birth</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Subject</th><th>Mark</th><th>Total</th><th>Average</th><th>University</th><th>Year</th></tr>"
 
   rollno=params[:rollno]
@@ -21,7 +21,7 @@ get '/result_info' do
       k= k +"<tr><td>"+ l.join("</td><td>") + "</td></tr>"
     end
   else
-    va="No such roll number exists!</table><center><br><br><a href=\'/result'\"><i>Back to home</i></a></form></body></html>"
+    va="<p>No such roll number exists!</p><center><br><br><a href=\'/result'\"><i>Back to home</i></a></form></body></html>"
     return va
   end
   k+="</table><center><br><br><a href=\'/result'\"><i>Back to home</i></a></form><p><ul>
@@ -33,7 +33,7 @@ end
 
 get '/result' do
 s="<html><head><title>University of Madras Result 2013 page</title></head>
-<body><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><center><br><h3>University of Madras Results 2013 (PG) - MCA</h3><br>
+<body><img src=\'/home/chandrasekar/Chan_Programs/mm.png\'></img><marquee bgcolor=\"yellow\"><font color=red><b>University of Madras PG Results 2013</b></font></marquee><center><br><h3>University of Madras Results 2013 (PG) - MCA</h3><br>
 <form id=\"login\" action=\"/result_info\" method=\"get\">
 <table border=\"1\" size=\"700\ height=\"500\">
 <tr><td align=\"center\"><br><br>
