4199a302785c5891f3f8cd7f5c198be30b700805
chandrasekaran.r@railsfactory.org <chandrasekaran.r@railsfactory.org>
2013-09-16 17:35:26 +0530

    Modified the structure definition for employee class and webpage file

--- shekar/16_September/employee_get.rb
+++ shekar/16_September/employee_get.rb
@@ -5,9 +5,24 @@ require 'mysql'
 require '~/training-ruby/shekar/16_September/employee'
 
 get '/get_input' do
-   s="<html><body><form ><br><div align=\"/center\">"
-   res=0 
-   s+="</div></form></body></html>"
+   s="<html><body><form ><br><div align=\"/center\"><table>"
+   res=0
+    
+   obj=Employee.new
+   id=params[:id]
+   name=params[:name]
+   desig=params[:desig]
+   grade=params[:grade]
+   qcc_team=params[:qcc_team]
+   qcc_expo=params[:qcc_expo]
+
+   res = obj.select(id)
+
+    ds.each do |l|
+      s= s +"<tr><td>"+ l.join("</td><td>") + "</td></tr>"
+    end
+   s+="</table></div><br><br><a href=\"anagram\">Back to previous page</a></form></center></body></html>"
+  return s
 end
 
 get '/employee' do  
