753fc6c72501e490b342315324f1560af3f265fa
railsfactory-rajalakshmi <rajalakshmis@railsfactory.org>
2013-09-16 16:51:26 +0530

    Tried erb and few programs

---
+++ lakshmi/16sept/prg5.rb
@@ -0,0 +1,12 @@
+s = ".sdrawkcab si gnirts sihT" 
+s.reverse 
+s 
+s.reverse! 
+puts s 
+
+str = "order. wrong the in are words These"
+puts str.split(/(\s+)/).reverse!.join('')
+
+puts str.split(/\b/).reverse!.join('')
+
+
