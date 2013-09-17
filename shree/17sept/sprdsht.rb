ddfb0d84e55377f2e6d0f1cf76d097b62cb23748
Marishree <marishree@railsfactory.org>
2013-09-17 17:59:09 +0530

    spreadsheet

---
+++ shree/17sept/sprdsht.rb
@@ -0,0 +1,18 @@
+require 'spreadsheet'
+Spreadsheet.client_encoding='UTF-8'
+book=Spreadsheet::Workbook.new
+sheet1=book.create_worksheet
+sheet2 = book.create_worksheet 
+ sheet1.row(0).push'name','designation','place'
+ sheet1.row(1).push'x','jse','ch'
+ sheet1.row(2).push'y','se','mum'
+ sheet1.row(3).push'z','pm','ban' 
+ sheet1.row(4).push'dm','del'
+ sheet1.row(4).insert 0,'a'
+ sheet1.row(2).replace [ 'b','tl','hyd']
+sheet2.row(0).push'num','name'
+sheet2.row(1).push'1','ms' 
+ sheet2.row(2).push'2','ra'
+ sheet2.row(3).insert 0,'a'
+ 
+ book.write '/home/manishree/training-ruby/shree/17sept/test.xls'
